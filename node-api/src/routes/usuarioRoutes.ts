db9236bb1ad5c8c63d03f01cfb7ac907674cbbde
Rodrigo Drummond Macedo <rodrigodm2810@gmail.com>
2024-07-25 17:48:56 -0300

    rota para deslogar usuário

--- node-api/src/routes/usuarioRoutes.ts
+++ node-api/src/routes/usuarioRoutes.ts
@@ -1,10 +1,24 @@
-import { Router } from 'express';
-import { createUser, getUsers } from '../controllers/usuarioController';
-import { authenticate } from '../shared/middleware/auth';
+import { Router, Request, Response } from "express";
+import { createUser, getUsers } from "../controllers/usuarioController";
+import { authenticate } from "../shared/middleware/auth";
+
+declare global {
+  namespace Express {
+    interface Request {
+      logout(): void;
+      flash(type: string, message: string): void;
+    }
+  }
+}
 
 const router = Router();
 
-router.post('/users', createUser);
-router.get('/users', getUsers); 
+router.post("/users", createUser);
+router.get("/users", getUsers);
+router.get("/users/logout", (req: Request, res: Response) => {
+  req.logout();
+  req.flash("success_msg", "Deslogado com sucesso!");
+  res.redirect("/");
+});
 
 export default router;
