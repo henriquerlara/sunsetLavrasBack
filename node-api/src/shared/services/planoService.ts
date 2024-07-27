d97c9de54dc58f619dbc247ae1ac7aa665aed83a
Tiago Lascasas <tiago-antunes@hotmail.com.br>
2024-07-27 19:26:25 -0300

    planoService

---
+++ node-api/src/shared/services/planoService.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import Plano from "../../models/plano";
+
+class PlanoService {
+  getPlanosByUserId = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      if (!id || isNaN(Number(id))) {
+        res.status(400).json({ error: 'ID is required or is not valid' });
+        return;
+    }
+      const planos = await Plano.findAll({
+        where: {
+          idUsuario: id
+        }
+      });
+      res.status(200).json(planos);
+    } catch (error) {
+      console.error("Error getting planos:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+}
+
+export default new PlanoService();
